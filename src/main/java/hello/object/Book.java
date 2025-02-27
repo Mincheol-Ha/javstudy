ba44bc6f668845cbad5a181ac95ac6db56a3e7e4
mincheolha <hmc0501@gmail.com>
2025-02-27 15:39:48 +0900

    20220227 11장 JDK 기본클래스 Object, String

---
+++ src/main/java/hello/object/Book.java
@@ -0,0 +1,17 @@
+package hello.object;
+
+public class Book {
+    int bookNumber;
+    String bookTitle;
+
+    public Book(int bookNumber, String bookTitle) {
+        this.bookNumber = bookNumber;
+        this.bookTitle = bookTitle;
+    }
+
+    @Override
+    public String toString() {
+        return bookTitle + ", " + bookNumber;
+    }
+}
+
---
+++ src/main/java/hello/object/ObjectColneTest.java
@@ -0,0 +1,17 @@
+package hello.object;
+
+public class ObjectColneTest {
+    public static void main(String[] args) throws CloneNotSupportedException {
+        Circle circle = new Circle(10, 20, 30);
+        Circle copyCircle = (Circle) circle.clone();
+
+
+        System.out.println(circle);
+        System.out.println(copyCircle);
+        System.out.println(System.identityHashCode(circle));
+        System.out.println(System.identityHashCode(copyCircle));
+
+
+
+    }
+}
