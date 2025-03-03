78363b847ff9e4af4ef77d514c91b56af68599cc
mincheolha <hmc0501@gmail.com>
2025-03-03 23:46:34 +0900

    20220303 13장 13-1 인스턴스 내부클래스, 지역 내부클래스

---
+++ src/main/java/hello/innerclass/LocalInnerTest.java
@@ -0,0 +1,9 @@
+package hello.innerclass;
+
+public class LocalInnerTest {
+    public static void main(String[] args) {
+        Outer out = new Outer();
+        Runnable runner = out.getRunnable(10);
+        runner.run();
+    }
+}
---
+++ src/main/java/hello/innerclass/Outer.java
@@ -0,0 +1,27 @@
+package hello.innerclass;
+
+public class Outer {
+    int outNum = 100;
+    static int sNum = 200;
+
+    Runnable getRunnable(int i) {
+        int num = 100;
+
+        class MyRunnable implements Runnable {
+            int localNum = 10;
+
+            @Override
+            public void run() {
+                // num = 200; 지역 변수는 상ㅅ수로 바뀌므로 값을 변결할 수 없어 오류 발생
+                // i = 100; 매개변수고 지역 변수처럼 상수로 바뀌므로 값을 변경할 수 없어 오류 발생
+                System.out.println("i = " + i);
+                System.out.println("num = " + num);
+                System.out.println("localNum = " + localNum);
+                System.out.println("outNum = " + outNum + "(외부 클래스 인스턴스 변수)");
+                System.out.println("Out.sNum = " + sNum + "(외부 클래스 정적 변수)");
+            }
+        }
+        return new MyRunnable();
+    }
+}
+
