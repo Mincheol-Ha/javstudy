dca8eac0adfe61169114d23c1392c4d005587439
mincheol.ha <hmc0501@gmail.com>
2025-03-07 14:11:32 +0900

    20220307 15장 입출력 스트림1

---
+++ src/main/java/hello/stream/inputstream/FileInputStreamTest3.java
@@ -0,0 +1,26 @@
+package hello.stream.inputstream;
+
+import ch.qos.logback.core.encoder.JsonEscapeUtil;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class FileInputStreamTest3 {
+    public static void main(String[] args) {
+        try (FileInputStream fis = new FileInputStream("input2.txt")) {
+            byte[] bs = new byte[10]; // 크기가 10인 바이트 배열 생성
+            int i;
+            while ((i = fis.read(bs)) != -1) {
+                for (int k = 0; k < i; k++) {
+                    System.out.print((char)bs[k]);
+            }
+            System.out.println(": " + i + "바이트 읽음");
+        }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println("end");
+
+    }
+}
