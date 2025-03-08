81af0c93254760d9a382c2798333fbb4dc67a68a
mincheol.ha <hmc0501@gmail.com>
2025-03-09 03:02:05 +0900

    20220308 15장 InputSteamReader

---
+++ src/main/java/hello/stream/decorator/InputSteamReaderTest.java
@@ -0,0 +1,18 @@
+package hello.stream.decorator;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class InputSteamReaderTest {
+    public static void main(String[] args) {
+        try(InputStreamReader isr = new InputStreamReader(new FileInputStream("reader.txt"))) {
+            int  i;
+            while ((i = isr.read()) != -1) {
+                System.out.print((char) i);
+            }
+        } catch(IOException e) {
+            System.out.println("파일을 찾을 수 없습니다. 경로를 확인하세요.");
+        }
+    }
+}
