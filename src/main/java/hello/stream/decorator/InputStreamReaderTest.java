1852298de99d1b7e3f66654de0c724143a462be7
mincheol.ha <hmc0501@gmail.com>
2025-03-09 12:39:47 +0900

    20220308 15장 InputStreamReader2

---
+++ src/main/java/hello/stream/decorator/InputStreamReaderTest.java
@@ -0,0 +1,16 @@
+package hello.stream.decorator;
+
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+
+public class InputStreamReaderTest {
+    public static void main(String[] args) {
+        try(InputStreamReader isr = new InputStreamReader(new FileInputStream("reader.txt"))) {
+            int i;
+            while ((i = isr.read()) != -1)
+                System.out.print((char) i);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
